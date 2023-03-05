a31495730327e9d260124309933c9e4e5e00dba0
ergofriend <erg.friend@gmail.com>
2023-03-05 11:16:00 +0900

    fix css

--- src/client/components/foundation/WidthRestriction/WidthRestriction.styles.ts
+++ src/client/components/foundation/WidthRestriction/WidthRestriction.styles.ts
@@ -1,10 +1,11 @@
 import { css } from '@emotion/css';
 
 export const container = () => css`
-  max-width: 1024px;
+  width: 100%;
 `;
 
 export const inner = () => css`
   margin: 0 auto;
+  max-width: 1024px;
   width: 100%;
 `;
