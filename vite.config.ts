e8d2600c9bbe4704749beaf7e37427360ad39193
ergofriend <erg.friend@gmail.com>
2023-03-05 09:17:09 +0900

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
@@ -7,34 +5,13 @@ import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 
-import { getFileList } from './tools/get_file_list';
-
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
-
-  return publicFiles;
-};
-
 export default defineConfig(async () => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
-
   return {
     build: {
-      assetsInlineLimit: 20480,
-      cssCodeSplit: false,
-      cssTarget: 'es6',
       minify: true,
       rollupOptions: {
-        output: {
-          experimentalMinChunkSize: 40960,
-        },
         plugins: [visualizer()],
       },
-      target: 'es2015',
     },
     plugins: [
       react(),
@@ -43,7 +20,6 @@ export default defineConfig(async () => {
       ViteEjsPlugin({
         module: '/src/client/index.tsx',
         title: '買えるオーガニック',
-        videos,
       }),
     ],
   };
