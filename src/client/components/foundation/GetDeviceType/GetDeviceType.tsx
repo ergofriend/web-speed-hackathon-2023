5f033bb83b8462cae39beb75dfa055b87561eb62
ergofriend <erg.friend@gmail.com>
2023-03-05 09:20:03 +0900

    ClassComponent to FunctionalComponent

--- src/client/components/foundation/GetDeviceType/GetDeviceType.tsx
+++ src/client/components/foundation/GetDeviceType/GetDeviceType.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 export const DeviceType = {
   DESKTOP: 'DESKTOP',
@@ -11,37 +11,23 @@ type Props = {
   children: ({ deviceType }: { deviceType: DeviceType }) => ReactNode;
 };
 
-export class GetDeviceType extends Component<Props> {
-  private _timer: number | null;
-  private _windowWidth: number;
+export const GetDeviceType = ({ children }: Props) => {
+  const [width] = useWindowSize();
+  const deviceType = width > 768 ? DeviceType.DESKTOP : DeviceType.MOBILE;
 
-  constructor(props: Props) {
-    super(props);
-    this._windowWidth = window.innerWidth;
-    this._timer = null;
-  }
+  return <>{children({ deviceType })}</>;
+};
 
-  componentDidMount(): void {
-    this._checkIsDesktop();
-  }
+const useWindowSize = (): number[] => {
+  const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+  useEffect(() => {
+    const updateSize = (): void => {
+      setSize([window.innerWidth, window.innerHeight]);
+    };
 
-  componentWillUnmount(): void {
-    if (this._timer != null) {
-      window.clearImmediate(this._timer);
-    }
-  }
+    window.addEventListener('resize', updateSize);
 
-  private _checkIsDesktop() {
-    this._windowWidth = window.innerWidth;
-    this.forceUpdate(() => {
-      this._timer = window.setImmediate(this._checkIsDesktop.bind(this));
-    });
-  }
-
-  render() {
-    const { children: render } = this.props;
-    return render({
-      deviceType: this._windowWidth >= 1024 ? DeviceType.DESKTOP : DeviceType.MOBILE,
-    });
-  }
-}
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+};
